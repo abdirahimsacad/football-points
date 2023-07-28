12b8ed7a577ca55d37315022119027ec92650d89
abdirahimsacad <abdirahiimsacad@gmail.com>
2023-07-28 08:20:25 -0700

    update

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function cal(win, draw, loss) {
+  return (win * 3) + (draw * 1) + (loss * 0);
+}
+
+console.log(cal(3, 2, 1));
+console.log(cal(5, 0, 2)); 
+console.log(cal(0, 1, 8)); 
